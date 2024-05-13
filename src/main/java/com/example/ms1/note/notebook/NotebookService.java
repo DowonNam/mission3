63a13d5c8c5ba6bf041b66d71c15d28db71d03ff
Dowon Nam <dwnam0114@gmail.com>
2024-05-13 14:45:35 +0900

    update function

---
+++ src/main/java/com/example/ms1/note/notebook/NotebookService.java
@@ -0,0 +1,37 @@
+package com.example.ms1.note.notebook;
+
+import com.example.ms1.note.MainDataDto;
+import com.example.ms1.note.note.Note;
+import com.example.ms1.note.note.NoteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NotebookService {
+    private final NotebookRepository notebookRepository;
+    public List<Notebook> getNotebookList(){
+        List<Notebook> notebookList = notebookRepository.findAll();
+        return notebookList;
+    }
+
+    public Notebook getNotebook(long id){
+        Notebook notebook = notebookRepository.findById(id).get();
+        return notebook;
+    }
+
+    public Notebook save(Notebook notebook) {
+        return notebookRepository.save(notebook);
+    }
+
+    public void delete(Long id){
+        notebookRepository.deleteById(id);
+    }
+    public void updateName(Long id, String name){
+        Notebook notebook = getNotebook(id);
+        notebook.setName(name);
+        notebookRepository.save(notebook);
+    }
+}
